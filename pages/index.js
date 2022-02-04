a5637d894c85ef5afff00c6e372f42a5423de0bf
Diego Fernando Alayon <dfar.41@gmail.com>
2022-02-04 11:32:09 -0500

    usando Link para que la pagina funcione como una spa

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
@@ -15,6 +16,13 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org" target='_blank'>Next.js!</a>
         </h1>
+        <nav>
+          <Link href='/timeline'>
+            <a>
+              timeline
+            </a>
+          </Link>
+        </nav>
 
       </main>
       <style jsx>
