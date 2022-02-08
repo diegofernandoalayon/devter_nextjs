515c484d2029f2366a73f8756fa108e8f86ed2d7
Diego Fernando Alayon <dfar.41@gmail.com>
2022-02-08 18:31:12 -0500

    extrayendo estilos de styled jsx a un archivo externo para limpiar componente

--- components/AppLayout/index.js
+++ components/AppLayout/index.js
@@ -1,6 +1,6 @@
-import {colors, breakpoints} from '../../styles/theme'
-import { addOpacityToColor } from '../../styles/utils'
-const backgroundColor = addOpacityToColor(colors.primary,0.2)
+
+
+import styles, {globlaStyles} from './styles'
 export default function AppLayout({children})  {
   return (
     <>
@@ -9,40 +9,8 @@ export default function AppLayout({children})  {
           {children}
         </main>
       </div>
-      <style jsx>{`
-        div {
-          display: grid;
-          place-items: center;
-          height: 100vh;
-        }
-        main {
-          background: #fff;
-          box-shadow: 0 10px 25px rgba(0,0,0, .1);
-          border-radius: 8px;
-          width: 100%;
-          height: 100%;
-        }
-        @media(min-width: ${breakpoints.mobile}){
-          main {
-            height: 90vh;
-            width: ${breakpoints.mobile};
-      
-          }
-        }
-      `}
-      </style>
-      <style jsx global>{`
-        html,
-        body {
-          background-image: radial-gradient( ${backgroundColor} 1px, transparent 1px), radial-gradient(${backgroundColor} 1px, transparent 1px) ;
-          background-position: 0 0, 25px 25px;
-          background-size: 50px 50px;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}
-      </style>
+      <style jsx>{styles}</style>
+      <style jsx global>{globlaStyles}</style>
     </>
   )
 }
---
+++ components/AppLayout/styles.js
@@ -0,0 +1,36 @@
+import css from 'styled-jsx/css'
+import {colors, breakpoints} from '../../styles/theme'
+import { addOpacityToColor } from '../../styles/utils'
+const backgroundColor = addOpacityToColor(colors.primary,0.2)
+export const globlaStyles = css.global`
+  html,
+  body {
+    background-image: radial-gradient( ${backgroundColor} 1px, transparent 1px), radial-gradient(${backgroundColor} 1px, transparent 1px) ;
+    background-position: 0 0, 25px 25px;
+    background-size: 50px 50px;
+  }
+  * {
+    box-sizing: border-box;
+  }
+`
+export default css`
+  div {
+    display: grid;
+    place-items: center;
+    height: 100vh;
+  }
+  main {
+    background: #fff;
+    box-shadow: 0 10px 25px rgba(0,0,0, .1);
+    border-radius: 8px;
+    width: 100%;
+    height: 100%;
+  }
+  @media(min-width: ${breakpoints.mobile}){
+    main {
+      height: 90vh;
+      width: ${breakpoints.mobile};
+
+    }
+  }
+`
