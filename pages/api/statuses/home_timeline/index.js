cc6886fe1c718df2c589b4850a0ef5cff4b5764f
Diego Fernando Alayon <dfar.41@gmail.com>
2022-02-22 23:18:39 -0500

    creando info de prueba para el endpoint home_timeline

--- pages/api/statuses/home_timeline/index.js
+++ pages/api/statuses/home_timeline/index.js
@@ -1,3 +1,26 @@
+const timeline = [
+  {
+    avatar:
+      'https://avatars.githubusercontent.com/u/79520423?s=96&v=4',
+    username: 'hola mundo',
+    message: 'Twitter web app runs es6+ for modern browsers, reducing the polyfill bundle size by 83%'
+  },
+  {
+    avatar:
+      'https://avatars.githubusercontent.com/u/79521033?s=96&v=4',
+    username: 'hola mundo',
+    name: 'el mio',
+    message: 'devter, es lo maximo del mundo, funciona de una manera'
+  },
+  {
+    avatar:
+      'https://avatars.githubusercontent.com/u/15933?s=96&v=4',
+    username: 'hola casa',
+    name: 'Diego f',
+    message: 'otro mensaje de prueba,para devter, para probar que funciona '
+  }
+]
+
 export default function handler (req, res) {
-  res.status(200).json({ username: 'Casa' })
+  res.status(200).json(timeline)
 }
