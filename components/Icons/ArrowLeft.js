27a28d056b9aac0fdf124d430a6faa6a64d37a04
Diego Fernando Alayon <dfar.41@gmail.com>
2022-03-03 21:43:42 -0500

    creando icon ArrowLeft

---
+++ components/Icons/ArrowLeft.js
@@ -0,0 +1,15 @@
+export default function SvgComponent (props) {
+  return (
+    <svg height={21} width={21} xmlns="http://www.w3.org/2000/svg" {...props}>
+      <g
+        fill="none"
+        fillRule="evenodd"
+        stroke="currentColor"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      >
+        <path d="M7.499 6.497 3.5 10.499l4 4.001M16.5 10.5h-13" />
+      </g>
+    </svg>
+  )
+}
