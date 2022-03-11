e4cb76f917ec06646de5f8b31334745fb651cdf3
Diego Fernando Alayon <dfar.41@gmail.com>
2022-03-11 00:08:19 -0500

    cambiando el getInitialProps, por el getServerProps

--- pages/status/[id].js
+++ pages/status/[id].js
@@ -12,14 +12,18 @@ export default function DevitPage (props) {
     </>
   )
 }
-DevitPage.getInitialProps = (context) => {
-  const { query, res } = context
-  const { id } = query
-  return fetch(`http://localhost:3000/api/devits/${id}`)
-    .then(apiResponse => {
-      if (apiResponse.ok) return apiResponse.json()
-      if (res) {
-        res.writeHead(301, { location: '/home' }).end()
-      }
-    })
+
+export async function getServerSideProps (context) {
+  // params, req, res, query => en el context
+  const { params, res } = context
+  const { id } = params
+  const apiResponse = await fetch(`http://localhost:3000/api/devits/${id}`)
+
+  if (apiResponse.ok) {
+    const props = await apiResponse.json()
+    return { props }
+  }
+  if (res) {
+    res.writeHead(301, { location: '/home' }).end()
+  }
 }
