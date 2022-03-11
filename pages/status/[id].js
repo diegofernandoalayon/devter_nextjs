2ecf9d513dae076616cee2943b0a628afdbf0fe6
Diego Fernando Alayon <dfar.41@gmail.com>
2022-03-11 14:03:17 -0500

    volviendo a getServerSideProps

--- pages/status/[id].js
+++ pages/status/[id].js
@@ -1,8 +1,8 @@
-import { firestore } from 'firebasee/admin'
+// import { firestore } from 'firebasee/admin'
 import Devit from 'components/Devit'
 import { useRouter } from 'next/router'
 export default function DevitPage (props) {
-  console.log(props.id)
+  // console.log(props.id)
   const router = useRouter()
   const { id } = router.query
   console.log(props)
@@ -14,49 +14,49 @@ export default function DevitPage (props) {
   )
 }
 
-// export async function getServerSideProps (context) {
-//   // params, req, res, query => en el context
-//   const { params, res } = context
-//   const { id } = params
-//   const apiResponse = await fetch(`http://localhost:3000/api/devits/${id}`)
+export async function getServerSideProps (context) {
+  // params, req, res, query => en el context
+  const { params, res } = context
+  const { id } = params
+  const apiResponse = await fetch(`http://localhost:3000/api/devits/${id}`)
 
-//   if (apiResponse.ok) {
-//     const props = await apiResponse.json()
-//     return { props }
-//   }
-//   if (res) {
-//     res.writeHead(301, { location: '/home' }).end()
-//   }
-// }
-export async function getStaticPaths () {
-  return {
-    paths: [{ params: { id: 'OvEyPwFo5We1JtSQSCyG' } }],
-    fallback: false
+  if (apiResponse.ok) {
+    const props = await apiResponse.json()
+    return { props }
+  }
+  if (res) {
+    res.writeHead(301, { location: '/home' }).end()
   }
 }
+// export async function getStaticPaths () {
+//   return {
+//     paths: [{ params: { id: 'OvEyPwFo5We1JtSQSCyG' } }],
+//     fallback: false
+//   }
+// }
 
-export async function getStaticProps (context) {
-  // params, req, res, query => en el context
-  const { params } = context
-  const { id } = params
+// export async function getStaticProps (context) {
+//   // params, req, res, query => en el context
+//   const { params } = context
+//   const { id } = params
 
-  return firestore
-    .collection('devits')
-    .doc(id)
-    .get()
-    .then(doc => {
-      const data = doc.data()
-      const id = doc.id
-      const { createdAt } = data
-      if (data) {
-        const props = {
-          ...data,
-          id,
-          createdAt: +createdAt.toDate()
-        }
-        return { props }
-      }
-    }).catch(() => {
-      return { props: {} }
-    })
-}
+//   return firestore
+//     .collection('devits')
+//     .doc(id)
+//     .get()
+//     .then(doc => {
+//       const data = doc.data()
+//       const id = doc.id
+//       const { createdAt } = data
+//       if (data) {
+//         const props = {
+//           ...data,
+//           id,
+//           createdAt: +createdAt.toDate()
+//         }
+//         return { props }
+//       }
+//     }).catch(() => {
+//       return { props: {} }
+//     })
+// }
