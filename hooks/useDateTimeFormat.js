ea440bf6604fc45dc8bc4311fc03e36ba1b42a00
Diego Fernando Alayon <dfar.41@gmail.com>
2022-03-16 23:30:13 -0500

    aplicando una mejora progresiva en el hook useDateTimeFormat para soportar navegadores antiguos que no poseean el objeto Intl para el formateo de las fechas

--- hooks/useDateTimeFormat.js
+++ hooks/useDateTimeFormat.js
@@ -2,8 +2,20 @@ const LANGUAGES = {
   DEFAULT_LANGUAGE: 'es-ES'
 }
 
-export default function useDateTimeFormat (timestamp) {
+const isDateTimeFormatSupported =
+  typeof Intl !== 'undefined' && Intl.DateTimeFormat
+
+export const formatDate = (timestamp, { language = LANGUAGES.DEFAULT_LANGUAGE }) => {
   const date = new Date(timestamp)
+  if (!isDateTimeFormatSupported) {
+    const options = {
+      weekday: 'short',
+      year: 'numeric',
+      month: 'short',
+      day: 'numeric'
+    }
+    return date.toLocaleDateString(language, options)
+  }
   const options = {
     year: 'numeric',
     month: 'numeric',
@@ -12,5 +24,8 @@ export default function useDateTimeFormat (timestamp) {
     minute: 'numeric',
     second: 'numeric'
   }
-  return new Intl.DateTimeFormat(LANGUAGES.DEFAULT_LANGUAGE, options).format(date)
+  return new Intl.DateTimeFormat(language, options).format(date)
+}
+export default function useDateTimeFormat (timestamp) {
+  return formatDate(timestamp, { language: LANGUAGES.DEFAULT_LANGUAGE })
 }
